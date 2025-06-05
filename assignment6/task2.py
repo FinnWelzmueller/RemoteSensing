363b892a5fae70de96d96d983fd8a2f42c9837cc
welz_fi <finn.welzmueller@dlr.de>
2025-06-05 12:19:36 +0200

    excluded uncertainties from fit

--- assignment6/task2.py
+++ assignment6/task2.py
@@ -6,7 +6,7 @@ import numpy as np
 def linear(m,x,b):
     return m*x + b
 def lin_fit(inp_x, inp_y, inp_y_err):
-    popt, pcov =  curve_fit(linear, inp_x, inp_y, sigma=inp_y_err, absolute_sigma=True)
+    popt, pcov =  curve_fit(linear, inp_x, inp_y)
     m, b = popt
     m_err, b_err = np.sqrt(np.diag(pcov))
     return m, m_err, b, b_err 
@@ -19,16 +19,16 @@ df_july = pd.read_csv("Sigma0_HH_july.csv", delim_whitespace=True)
 
 
 print("----- Statistics for May-----")
-print(f"Mean Sigma0: {np.around(np.mean(df_may['Sigma0_HH_mean']),4)}")
-print(f"Standard deviation Sigma0: {np.around(np.std(df_may['Sigma0_HH_mean']),4)}")
+print(f"Mean Sigma0: {np.mean(df_may['Sigma0_HH_mean'])}")
+print(f"Standard deviation Sigma0: {np.std(df_may['Sigma0_HH_mean'])}")
 print("----- Statistics for July-----")
-print(f"Mean Sigma0: {np.around(np.mean(df_july['Sigma0_HH_mean']),4)}")
-print(f"Standard deviation Sigma0: {np.around(np.std(df_july['Sigma0_HH_mean']),4)}")
+print(f"Mean Sigma0: {np.mean(df_july['Sigma0_HH_mean'])}")
+print(f"Standard deviation Sigma0: {np.std(df_july['Sigma0_HH_mean'])}")
 
 m_m, m_err_m, b_m, b_err_m = lin_fit(df_may["pixel_no"], df_may["Sigma0_HH_mean"], df_may["Sigma0_HH_sigma"])
 print("----- fitting results for May -----")
-print(f"Slope: {np.around(m_m,4)} +- {np.around(m_err_m,4)}")
-print(f"Intercept: {np.around(b_m,4)} +- {np.around(b_err_m,4)}")
+print(f"Slope: {m_m} +- {m_err_m}")
+print(f"Intercept: {b_m} +- {b_err_m}")
 
 fig, ax = plt.subplots(nrows=2)
 ax[0].plot(df_may["pixel_no"], df_may["Sigma0_HH_mean"], 'b.', label="Data")
@@ -41,8 +41,8 @@ ax[0].grid()
 
 m_j, m_err_j, b_j, b_err_j = lin_fit(df_july["pixel_no"], df_july["Sigma0_HH_mean"], df_july["Sigma0_HH_sigma"])
 print("----- fitting results for July -----")
-print(f"Slope: {np.around(m_j,4)} +- {np.around(m_err_j,4)}")
-print(f"Intercept: {np.around(b_j,4)} +- {np.around(b_err_j,4)}")
+print(f"Slope: {m_j} +- {m_err_j}")
+print(f"Intercept: {b_j} +- {b_err_j}")
 
 ax[1].plot(df_july["pixel_no"], df_july["Sigma0_HH_mean"], 'b.', label="Data")
 ax[1].plot(df_july["pixel_no"], [linear(m_j, ele, b_j) for ele in df_july["pixel_no"]], 'r--', label="Linear fit")
