9eff8e0b7170800b881cbc812f92bf47339de0c5
welz_fi <finn.welzmueller@dlr.de>
2025-07-06 12:19:19 +0200

    added code for the correlation plots

---
+++ assignment10/correlate.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+
+df = pd.read_csv("./correlation-data.csv", delim_whitespace=True)
+
+# plot with outlier
+r = np.around(pearsonr(df["Chla_[mg/m3]_ref"], df["CHL1_mean_mean"])[0],3)
+print(r)
+fig, ax = plt.subplots()
+#ax.grid()
+ax.scatter(df["Chla_[mg/m3]_ref"], df["CHL1_mean_mean"], color='blue')
+ax.set_xlabel(r"Chla [mg/m$^3$]")
+ax.set_ylabel(r"CHL1 [mg/m$^3$]")
+ax.set_xlim(0,1.3)
+ax.set_ylim(0,12.5)
+ax.annotate(fr"$r = {r}$", xy=(0.1,0.9),xycoords='axes fraction', fontsize=14)
+fig.savefig("correlation-plot.png")
+
+#plot without outlier (station 12)
+df = pd.read_csv("./correlation-data-no-outlier.csv", delim_whitespace=True)
+r = np.around(pearsonr(df["Chla_[mg/m3]_ref"], df["CHL1_mean_mean"])[0],3)
+print(r)
+fig, ax = plt.subplots()
+#ax.grid()
+ax.scatter(df["Chla_[mg/m3]_ref"], df["CHL1_mean_mean"], color='blue')
+ax.set_xlabel(r"Chla [mg/m$^3$]")
+ax.set_ylabel(r"CHL1 [mg/m$^3$]")
+ax.set_xlim(0,1.3)
+ax.set_ylim(0,3)
+
+ax.annotate(fr"$r = {r}$", xy=(0.1,0.9),xycoords='axes fraction', fontsize=14)
+fig.savefig("correlation-plot-no-outlier.png")
