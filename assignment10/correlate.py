88077da15b585ed42bbc85407edc6de51aa8e11c
Finn <finnwelzmueller97@gmail.com>
2025-07-12 18:55:27 +0200

    added script for correlations for assignment 10

--- assignment10/correlate.py
+++ assignment10/correlate.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 
-df = pd.read_csv("./correlation-data.csv", delim_whitespace=True)
+df = pd.read_csv("./correlation-data-eustrophic.csv", delim_whitespace=True)
 
 # plot with outlier
 r = np.around(pearsonr(df["Chla_[mg/m3]_ref"], df["CHL1_mean_mean"])[0],3)
@@ -17,10 +17,10 @@ ax.set_xlim(0,1.3)
 ax.set_ylim(0,12.5)
 ax.annotate(fr"$r = {r}$", xy=(0.1,0.9),xycoords='axes fraction', fontsize=14,bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.3'))
 ax.grid()
-fig.savefig("correlation-plot.png")
+fig.savefig("correlation-plot-eustrophic.png")
 
 #plot without outlier (station 12)
-df = pd.read_csv("./correlation-data-no-outlier.csv", delim_whitespace=True)
+df = pd.read_csv("./correlation-data-mesotrophic.csv", delim_whitespace=True)
 r = np.around(pearsonr(df["Chla_[mg/m3]_ref"], df["CHL1_mean_mean"])[0],3)
 fig, ax = plt.subplots()
 #ax.grid()
@@ -32,4 +32,4 @@ ax.grid()
 ax.set_ylim(0,3)
 
 ax.annotate(fr"$r = {r}$", xy=(0.1,0.9),xycoords='axes fraction', fontsize=14, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.3'))
-fig.savefig("correlation-plot-no-outlier.png")
+fig.savefig("correlation-plot-mesotrophic.png")
